41936c6fbcf415d0a79dab64483eba128f821f57
ThibautFgi <thibaut.fgi@hotmail.com>
2026-04-13 13:12:27 +0200

    maj

--- update-submodules-develop.sh
+++ update-submodules-develop.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 set -eu
 
-# Ensure submodules are present and up to date, then switch each to develop tracking origin/develop.
+# Aligne la config des submodules et initialise ceux manquants.
+git submodule sync --recursive
 git submodule update --init --recursive
 
 git submodule foreach --recursive '
-  git fetch origin develop
-  if git show-ref --verify --quiet refs/heads/develop; then
-    git checkout develop
+  branch=develop
+  git fetch origin "$branch"
+  if git show-ref --verify --quiet "refs/heads/$branch"; then
+    git checkout "$branch"
   else
-    git checkout -b develop origin/develop
+    git checkout -b "$branch" "origin/$branch"
   fi
-  git pull --ff-only origin develop
+  git pull --ff-only origin "$branch"
 '
